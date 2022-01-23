9e223feecfd8bfd8f0a6176475ceb482b6238666
wiily <willyhuang18@gmail.com>
2022-01-22 22:54:01 -0800

    trying inquirer to load the function

--- Develop/index.js
+++ Develop/index.js
@@ -73,7 +73,13 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt(questions)
+    .then(function(data){
+        console.log(data);
+    })
+}
 
 // Function call to initialize app
 init();
